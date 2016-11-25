75ba2906444161aa0877db27e96bf5c20ab93c7a
Taqi Jaffri <tjaffri@gmail.com>
2016-11-25 02:37:14 -0600

    Reverting string change to test CI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,7 +23,7 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Chapters', component: ListPage },
-      { title: 'User Profile', component: ProfilePage }
+      { title: 'Profile', component: ProfilePage }
     ];
 
   }
